24b1ad4fa95936229fecdc5103398cf8d6ed1a13
yepes0 <71985093+yepes0@users.noreply.github.com>
2025-07-26 23:07:05 +0000

    initial commit

--- umbrella.rb
+++ umbrella.rb
@@ -1,1 +1,18 @@
 # Write your solution below!
+require "http"
+require "json"
+require "dotenv/load"
+
+require "http"
+
+# Hidden variables
+pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_API_KEY")
+
+# Assemble the full URL string by adding the first part, the API token, and the last part together
+pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/41.8887,-87.6355"
+
+# Place a GET request to the URL
+response = HTTP.get(pirate_weather_url)
+
+# Print the body of the response
+puts response.to_s
